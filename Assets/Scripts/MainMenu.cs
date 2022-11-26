0222ff9ba3909ae73ed200ade43c0b62a47cbedd
Kenion <zerrayX@gmail.com>
2022-11-26 18:06:10 +0100

    Added highscore to title screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,10 +7,19 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Button startButton;
     [SerializeField] private TMP_InputField nameField;
+    [SerializeField] private TextMeshProUGUI highscoreText;
 
     private void Start()
     {
         startButton.onClick.AddListener(StartBtnClicked);
+        var pManager = PersistentManager.Instance;
+        if (pManager.highScorePlayerName == null)
+        {
+            highscoreText.text = "";
+            return;
+        }
+        
+        highscoreText.text = $"Best Score: {pManager.highScorePlayerName}({pManager.HighScore})";
     }
 
     private void StartBtnClicked()
